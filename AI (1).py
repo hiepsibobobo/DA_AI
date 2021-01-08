4c2976148957ed09612d18423c59e5f2df0281d8
hiepsibobobo <77146246+hiepsibobobo@users.noreply.github.com>
2021-01-08 15:58:44 +0700

    Add files via upload

---
+++ AI (1).py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+df = pd.read_csv("D:\Downloads/players_20.csv")
+
+
+# In[3]:
+
+
+df.head()
+
+
+# In[4]:
+
+
+df.describe().columns
+
+
+# In[5]:
+
+
+df = df[['short_name','age', 'height_cm', 'weight_kg', 'overall', 'potential',
+'value_eur', 'wage_eur', 'international_reputation', 'weak_foot',
+'skill_moves', 'release_clause_eur', 'team_jersey_number',
+'contract_valid_until', 'nation_jersey_number', 'pace', 'shooting',
+'passing', 'dribbling', 'defending', 'physic', 'gk_diving',
+'gk_handling', 'gk_kicking', 'gk_reflexes', 'gk_speed',
+'gk_positioning', 'attacking_crossing', 'attacking_finishing',
+'attacking_heading_accuracy', 'attacking_short_passing',
+'attacking_volleys', 'skill_dribbling', 'skill_curve',
+'skill_fk_accuracy', 'skill_long_passing', 'skill_ball_control',
+'movement_acceleration', 'movement_sprint_speed', 'movement_agility',
+'movement_reactions', 'movement_balance', 'power_shot_power',
+'power_jumping', 'power_stamina', 'power_strength', 'power_long_shots',
+'mentality_aggression', 'mentality_interceptions',
+'mentality_positioning', 'mentality_vision', 'mentality_penalties',
+'mentality_composure', 'defending_marking', 'defending_standing_tackle',
+'defending_sliding_tackle', 'goalkeeping_diving',
+'goalkeeping_handling', 'goalkeeping_kicking',
+'goalkeeping_positioning', 'goalkeeping_reflexes']]
+
+
+# In[6]:
+
+
+df.head()
+
+
+# In[7]:
+
+
+df = df[df.overall > 86] # extracting players with overall above 86
+df
+
+
+# In[8]:
+
+
+
+df.isnull().sum()
+
+
+# In[9]:
+
+
+df = df.fillna(df.mean())
+
+
+# In[10]:
+
+
+df.isnull().sum()
+
+
+# In[11]:
+
+
+names = df.short_name.tolist()
+df = df.drop(['short_name'],axis = 1)
+
+
+# In[12]:
+
+
+df.head()
+
+
+# In[13]:
+
+
+from sklearn import preprocessing
+x = df.values # numpy array
+scaler = preprocessing.MinMaxScaler()
+x_scaled = scaler.fit_transform(x)
+X_norm = pd.DataFrame(x_scaled)
+
+
+# In[14]:
+
+
+from sklearn.decomposition import PCA
+pca = PCA(n_components = 2) # 2D PCA for the plot
+reduced = pd.DataFrame(pca.fit_transform(X_norm))
+
+
+# In[15]:
+
+
+from sklearn.cluster import KMeans
+# specify the number of clusters
+kmeans = KMeans(n_clusters=5)
+# fit the input data
+kmeans = kmeans.fit(reduced)
+# get the cluster labels
+labels = kmeans.predict(reduced)
+# centroid values
+centroid = kmeans.cluster_centers_
+# cluster values
+clusters = kmeans.labels_.tolist()
+
+
+# In[16]:
+
+
+reduced['cluster'] = clusters
+reduced['name'] = names
+reduced.columns = ['x', 'y', 'cluster', 'name']
+reduced.head(54)
+
+
+# In[17]:
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[18]:
+
+
+sns.set(style="whitegrid")
+ax = sns.lmplot(x="x", y="y", hue='cluster', data = reduced, legend=False,
+fit_reg=False, size = 15, scatter_kws={"s": 250})
+texts = []
+for x, y, s in zip(reduced.x, reduced.y, reduced.name):
+    texts.append(plt.text(x, y, s))
+ax.set(ylim=(-2, 2))
+plt.tick_params(labelsize=15)
+plt.xlabel("PC 1", fontsize = 20)
+plt.ylabel("PC 2", fontsize = 20)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
---
+++ AI-Copy7.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+df = pd.read_csv("D:\Downloads/players_20.csv")
+
+
+# In[3]:
+
+
+df.head()
+
+
+# In[4]:
+
+
+df.describe().columns
+
+
+# In[5]:
+
+
+df = df[['short_name', 'height_cm', 'weight_kg']]
+
+
+# In[6]:
+
+
+df.head()
+
+
+# In[7]:
+
+
+df = df[df.weight_kg > 96] # extracting players with overall above 86
+df
+
+
+# In[8]:
+
+
+
+df.isnull().sum()
+
+
+# In[9]:
+
+
+df = df.fillna(df.mean())
+
+
+# In[10]:
+
+
+df.isnull().sum()
+
+
+# In[11]:
+
+
+names = df.short_name.tolist()
+df = df.drop(['short_name'],axis = 1)
+
+
+# In[12]:
+
+
+df.head()
+
+
+# In[13]:
+
+
+from sklearn import preprocessing
+x = df.values # numpy array
+scaler = preprocessing.MinMaxScaler()
+x_scaled = scaler.fit_transform(x)
+X_norm = pd.DataFrame(x_scaled)
+
+
+# In[14]:
+
+
+from sklearn.decomposition import PCA
+pca = PCA(n_components = 2) # 2D PCA for the plot
+reduced = pd.DataFrame(pca.fit_transform(X_norm))
+
+
+# In[15]:
+
+
+from sklearn.cluster import KMeans
+# specify the number of clusters
+kmeans = KMeans(n_clusters=8)
+# fit the input data
+kmeans = kmeans.fit(reduced)
+# get the cluster labels
+labels = kmeans.predict(reduced)
+# centroid values
+centroid = kmeans.cluster_centers_
+# cluster values
+clusters = kmeans.labels_.tolist()
+
+
+# In[16]:
+
+
+reduced['cluster'] = clusters
+reduced['name'] = names
+reduced.columns = ['x', 'y', 'cluster', 'name']
+reduced.head(54)
+
+
+# In[17]:
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[18]:
+
+
+sns.set(style="whitegrid")
+ax = sns.lmplot(x="x", y="y", hue='cluster', data = reduced, legend=False,
+fit_reg=False, size = 15, scatter_kws={"s": 250})
+texts = []
+for x, y, s in zip(reduced.x, reduced.y, reduced.name):
+    texts.append(plt.text(x, y, s))
+ax.set(ylim=(-2, 2))
+plt.tick_params(labelsize=15)
+plt.xlabel("PC 1", fontsize = 20)
+plt.ylabel("PC 2", fontsize = 20)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
